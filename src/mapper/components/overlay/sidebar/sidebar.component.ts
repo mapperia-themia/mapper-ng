765169744f7d911b433f98e8b0c8042146b65b42
sefatanam <sefatanam@gmail.com>
2021-01-12 21:09:37 +0600

    sidebar button direction from config

--- src/mapper/components/overlay/sidebar/sidebar.component.ts
+++ src/mapper/components/overlay/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import {Navigation} from '../../../models/navigation';
+import {Config} from "../../../utils/config";
 
 @Component({
   selector: 'sidebar',
@@ -9,7 +10,7 @@ import {Navigation} from '../../../models/navigation';
 export class SidebarComponent implements OnInit {
   @Input() navs: Navigation[];
   @Input() isVisible = false;
-  @Input() direction: 'left' | 'right' | 'top' | 'bottom' = 'left';
+  @Input() direction: Config['position'] = 'left';
   @Input() transition: 'overlay' | 'uncover' | 'scale down' | 'push' | 'slide along' | 'slide out' = 'uncover';
   @Input() dimmedContent = false;
   @Output() visibilityChange = new EventEmitter<boolean>();
