0a343633588dea697833f66dc416e2c0af31e3be
lifeoflikhon <mehedi.hasansjs@gmail.com>
2021-01-15 20:24:01 +0600

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,21 @@ import {SidebarModule} from '../mapper/components/overlay/sidebar/sidebar.module
 import {ButtonModule} from '../mapper/components/button/button.module';
 import {DividerModule} from '../mapper/components/panel/divider/divider.module';
 import {FlagModule} from '../mapper/components/misc/flag/flag.module';
+import {HeaderModule} from '../mapper/components/panel/header/header.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SidebarModule,
-    ButtonModule,
-    DividerModule,
-    FlagModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SidebarModule,
+        ButtonModule,
+        DividerModule,
+        FlagModule,
+        HeaderModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
