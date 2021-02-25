53ee6db79d094b02cb8dda7ced504b5c3eae9034
lifeoflikhon <mehedi.hasansjs@gmail.com>
2021-02-25 16:23:14 +0600

    added some array method

--- src/app/helpers/utils/index.ts
+++ src/app/helpers/utils/index.ts
@@ -62,4 +62,69 @@ export class Utils {
       array.splice(array.indexOf(item), 1);
     }
   }
+
+  public static isEmptyArray(array: any[]): boolean {
+    return !Array.isArray(array) || array.length === 0;
+  }
+
+  public static getActiveElement(): Element {
+    return document.activeElement;
+  }
+
+  public static isAllElementsLoaded(): boolean {
+    let loaded = false;
+    document.addEventListener('DOMContentLoaded', () => {
+      loaded = true;
+    });
+    return loaded;
+  }
+
+  public static replaceAllUnloadedImageWithDefaultImage(defaultImageSource: string): void {
+    const allImages = document.getElementsByTagName('img');
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < allImages.length; i++) {
+      allImages[i].src = defaultImageSource;
+      allImages[i].alt = 'Image is not set';
+    }
+  }
+
+  public static emptyArrayByKeepingReference(array: any[]): void {
+    if (!this.isEmptyArray(array)) {
+      try {
+        array = [];
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      throw new Error('Array is already empty');
+    }
+  }
+
+  public static emptyArrayAndReturnRemovedItems(array: any[]): any[] {
+    return array.splice(0, array.length);
+  }
+
+  public static removeArrayByNotKeepingReference(array: any[]): void {
+    array.length = 0;
+  }
+
+  public static removePropertyFromObject(object: any, property: string, forever: boolean = false): void {
+    if (forever) {
+      this.removePropertyFromObjectForever(object, property);
+    } else {
+      object[property] = undefined;
+    }
+  }
+
+  public static removePropertyFromObjectForever(object: any, property: string): void {
+    delete object[property];
+  }
+
+  public static deleteFromArrayByIndex(array: any[], fromIndexToDelete: number, deleteCount: number = 1): void {
+    array.splice(fromIndexToDelete, deleteCount);
+  }
+
+  public static deleteFromArrayByIndexAndReturn(array: any[], fromIndexToDelete: number, deleteCount: number = 1): any[] {
+    return array.splice(fromIndexToDelete, deleteCount);
+  }
 }
